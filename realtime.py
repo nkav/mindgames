659a9523c1a4c19b1bb389cafe1706feb5eae679
nkav <nakavthekar@gmail.com>
2014-05-02 05:30:25 -0400

    3 working channels & fft

--- realtime.py
+++ realtime.py
@@ -1,37 +1,31 @@
 import serial
 import sys
 
-def data(num):
+def data():
   times = []
   fftdata1 = []
   fftdata2 = []
   fftdata3 = []
   ser = serial.Serial('/dev/tty.usbmodem1412')
-  ### Dump remaining data from previous runs
-  firstfound = False
-  while (not firstfound):
-    line = ser.readline() 
-    try:
-      values = [float(i) for i in line.rstrip('\r\n').split(',')]
-      if(len(values) == 4):
-        if(values[0] < 0.02):
-          times.append(values[0])
-          fftdata1.append(values[1])
-          fftdata2.append(values[2])
-          fftdata3.append(values[3])
-          firstfound = True
-          yield values
-    except:
-      pass 
   ### Continue collecting sampled data
-  try:
-    for n in xrange(num+10):
-      line = ser.readline() 
-      try:
-        values = [float(i) for i in line.rstrip('\r\n').split(',')]
-        if(len(values) == 4):
-          yield values
-      except:
-        pass
-  except GeneratorExit:
-    pass
+  start = False
+  while not start: 
+    line = ser.readline() 
+    if line.rstrip('\r\n') == 'start!':
+      start = True
+
+  line = ser.readline() 
+  t = [float(i) for i in line.rstrip('\r\n').split(',')]
+  line = ser.readline() 
+  ch1 = [float(i) for i in line.rstrip('\r\n').split(',')]
+  line = ser.readline() 
+  ch2 = [float(i) for i in line.rstrip('\r\n').split(',')]
+  line = ser.readline() 
+  ch3 = [float(i) for i in line.rstrip('\r\n').split(',')]
+
+  times = times + t
+  fftdata1 = fftdata1 + ch1
+  fftdata2 = fftdata2 + ch2
+  fftdata3 = fftdata3 + ch3
+
+  return [times, fftdata1, fftdata2, fftdata3] 
--- serialtest.py
+++ serialtest.py
@@ -9,101 +9,44 @@ import matplotlib.animation as animation
 from matplotlib import gridspec
 from realtime import data 
 
-#############################
-###Create plot and subplot###
-#############################
-
-# fig1 = plt.figure()
-# 
-# ax1 = plt.subplot2grid((3,4), (0,0), colspan=2)
-# ax2 = plt.subplot2grid((3,4), (1,0), colspan=2, sharex=ax1)
-# ax3 = plt.subplot2grid((3,4), (2,0), colspan=2, sharex=ax1)
-# ax4 = plt.subplot2grid((3,4), (0,2), colspan=2, rowspan=2)
-
-#####################
-###Initialize Axes###
-#####################
-
-# xdata  = []
-# y1data = []
-# y2data = []
-# y3data = []
-# 
-# line1, = ax1.plot(xdata, y1data, 'r-') 
-# line2, = ax2.plot(xdata, y2data, 'b-') 
-# line3, = ax3.plot(xdata, y3data, 'g-') 
-# 
-# for axn in [ax1, ax2, ax3]:
-#   axn.set_xlabel('Time (s)')
-#   axn.set_ylabel('Amplitude (mV)')
-#   axn.grid(True)
-#   axn.set_title('RT EEG (Time Domain)')
-#   axn.axis([0, 2, 0, 5000])
-
-def update_line(num):
-  t,y1, y2, y3 = sampledata[0][num], sampledata[1][num], sampledata[2][num], sampledata[3][num]
-  xdata.append(t)
-  y1data.append(y1)
-  y2data.append(y2)
-  y3data.append(y3)
-  line1.set_data(xdata,y1data)
-  line2.set_data(xdata,y2data)
-  line3.set_data(xdata,y3data)
-#   xmin, xmax = ax.get_xlim()
-#   if t >= xmax:
-#       ax.set_xlim(xmin, 2*xmax)
-#       ax.figure.canvas.draw()
-  return [line1, line2, line3] 
-
-def setup():
-  plt.ion()
-  plt.xlim(0,5)
-  plt.ylim(0,5000)
-  plt.plot(0,0)
-
-def run(niter=100,doblit=True):
-  sampledata = data(105) #generator
-  time, ch1, ch2, ch3 = sampledata.next()
-  plt.plot(time, ch2, 'bo')
-  for ii in xrange(niter):
-    print ii
-    try:
-      time, ch1, ch2, ch3 = sampledata.next()
-      print time, ch2
-      tm.sleep(0.10)
-      plt.plot(time,ch2,'bo')
-    except StopIteration:
-      break
-    except RuntimeError:
-      break
-
 def realfft(sampledata):
   return np.absolute(fft.rfft(sampledata))
 
 if __name__ == '__main__':
-  print "Hi!"
-  setup()
-  raw_input = raw_input("Enter to continue...")
-  run() 
-  plt.show()
-#   sampledata = data(105) 
-# 
-#   line1_ani = animation.FuncAnimation(fig1, update_line, 100, 
-#       interval=50, blit=True, repeat=False) 
-# 
-#   datasize = len(sampledata[0])
-#   timestep = sampledata[0][1] - sampledata[0][0] 
-#   freqfft = fft.fftfreq(len(sampledata[0]), d=timestep)
-#   fft1 = realfft(sampledata[1])
-#   fft2 = realfft(sampledata[2])
-#   fft3 = realfft(sampledata[3])
-# 
-#   ax4.set_xlabel('Frequency (Hz)')
-#   ax4.set_ylabel('Intensity')
-#   ax4.grid(True)
-#   ax4.set_title('RT EEG (Freq Domain)')
-#   ax4.axis([0, 20, 0, 86532])
-#   fft2[0] = 0.0
-#   ax4.plot(freqfft[:50], fft2[:50], 'r--') 
-#   fig1.tight_layout()
-#   plt.show()
+  plt.ion()
+  fig1 = plt.figure()
+  ax1 = plt.subplot2grid((3,4), (0,0), colspan=2)
+  ax2 = plt.subplot2grid((3,4), (1,0), colspan=2, sharex=ax1)
+  ax3 = plt.subplot2grid((3,4), (2,0), colspan=2, sharex=ax1)
+  ax4 = plt.subplot2grid((3,4), (0,2), colspan=2, rowspan=2)
+  for axn in [ax1, ax2, ax3]:
+    axn.set_xlabel('Time (s)')
+    axn.set_ylabel('Amplitude (V)')
+    axn.grid(True)
+    axn.set_title('RT EEG (Time Domain)')
+  ax4.set_xlabel('Frequency (Hz)')
+  ax4.set_ylabel('Intensity')
+  ax4.grid(True)
+  ax4.set_title('RT EEG (Freq Domain)')
+  ax4.axis([0, 30, 0, 15])
+  fig1.tight_layout()
+  plt.draw()
+  while(1):
+    sampledata = data() 
+    timestart = sampledata[0][0]
+    timeend = sampledata[0][-1]
+    for axn in [ax1, ax2, ax3]:
+      axn.axis([timestart, timeend, 0, 3.5])
+    l1, =  ax1.plot(sampledata[0], [3*i for i in sampledata[1]], 'r-')
+    l2, = ax2.plot(sampledata[0], [3*i for i in sampledata[2]], 'b-')
+    l3, = ax3.plot(sampledata[0], [3*i for i in sampledata[3]], 'g-')
+    datasize = len(sampledata[0])
+    timestep = sampledata[0][1] - sampledata[0][0] 
+    freqfft = fft.fftfreq(len(sampledata[0]), d=timestep)
+    fftdat = realfft(sampledata[2])
+    fftdat[0] = 0.0
+    l4, = ax4.plot(freqfft[:40], fftdat[:40], 'b--') 
+    fig1.tight_layout()
+    plt.draw()
+    for l in [l1, l2, l3, l4]:
+      l.remove()
