f9dde04e14cce7a04952a880ccd3e5ac2473477d
nkav <nakavthekar@gmail.com>
2014-05-08 20:15:54 -0400

    changed name of mindgames

--- realtime.py
+++ realtime.py
@@ -1,35 +1,41 @@
 import serial
 import sys
 
-SERIAL_PORT = '/dev/ttyACM0'
+SERIAL_PORT = '/dev/tty.usbmodem1412'
 
 def data():
   ser = serial.Serial(SERIAL_PORT)
-  while(1):
-    times = []
-    fftdata1 = []
-    fftdata2 = []
-    fftdata3 = []
-    ### Continue collecting sampled data
-    start = False
-    while not start: 
-      line = ser.readline() 
-      if line.rstrip('\r\n') == 'start!':
-        start = True
-    try:
-      line = ser.readline() 
-      t = [float(i) for i in line.rstrip('\r\n').split(',')]
-      line = ser.readline() 
-      ch1 = [float(i) for i in line.rstrip('\r\n').split(',')]
-      line = ser.readline() 
-      ch2 = [float(i) for i in line.rstrip('\r\n').split(',')]
-      line = ser.readline() 
-      ch3 = [float(i) for i in line.rstrip('\r\n').split(',')]
-      times = times + t
-      fftdata1 = fftdata1 + ch1
-      fftdata2 = fftdata2 + ch2
-      fftdata3 = fftdata3 + ch3
+  ser.readline()
+  ser.flush()
+  start = False
+  while not start: 
+    line = ser.readline() 
+    print "Stuck waiting for start"
+    if line.rstrip('\r\n') == 'start!':
+      print "Line is start!"
+      start = True
+    else:
+      print line
+  times = []
+  fftdata1 = []
+  fftdata2 = []
+  fftdata3 = []
+  ### Continue collecting sampled data
+  ser.flush()
+  try:
+    line = ser.readline() 
+    t = [float(i) for i in line.rstrip('\r\n').split(',')]
+    line = ser.readline() 
+    ch1 = [float(i) for i in line.rstrip('\r\n').split(',')]
+    line = ser.readline() 
+    ch2 = [float(i) for i in line.rstrip('\r\n').split(',')]
+    line = ser.readline() 
+    ch3 = [float(i) for i in line.rstrip('\r\n').split(',')]
+    times = times + t
+    fftdata1 = fftdata1 + ch1
+    fftdata2 = fftdata2 + ch2
+    fftdata3 = fftdata3 + ch3
 
-      return [times, fftdata1, fftdata2, fftdata3] 
-    except:
-      pass  
+    return [times, fftdata1, fftdata2, fftdata3] 
+  except:
+    pass  
